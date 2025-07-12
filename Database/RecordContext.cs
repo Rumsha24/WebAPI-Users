1b2db2e20980c601702e813e81d98a34e7ec0376
Rumsha24 <rumsha26@gmail.com>
2025-07-12 00:57:59 -0400

    Initial commit

---
+++ Database/RecordContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using UsersAPI.Models;
+
+namespace UsersAPI.Database
+{
+    public class RecordContext : DbContext
+    {
+        public RecordContext(DbContextOptions<RecordContext> options) : base(options) { }
+
+        public DbSet<Record> Records { get; set; } = null!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersAPI.Database;
+using UsersAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//  Use SQL Server DB (not InMemory)
+builder.Services.AddDbContext<RecordContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//  Seed data on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<RecordContext>();
+    db.Database.EnsureCreated();
+
+    // Only seed if empty
+    if (!db.Records.Any())
+    {
+        db.Records.AddRange(
+            new Record
+            {
+                Name = "Ali Raza",
+                Email = "ali@example.com",
+                PhoneNumber = "03001234567",
+                DateOfBirth = new DateTime(1995, 4, 21)
+            },
+            new Record
+            {
+                Name = "Sara Ahmed",
+                Email = "sara@example.com",
+                PhoneNumber = "03111234567",
+                DateOfBirth = new DateTime(1997, 6, 15)
+            }
+        );
+        db.SaveChanges();
+    }
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
